118c2d5212c4d507e3e3f92e8e6a74aacf873a56
Dmitry <dsuschinsky@gmail.com>
2023-11-23 14:16:41 +0300

    fix editor for news

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -31,6 +31,33 @@ Trestle.configure do |config|
     c.convert_urls = false
   end
 
+  config.tinymce.default.configure do |c|
+    c.file_picker_types = "image"
+    c.file_picker_callback = "function (cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+
+      input.onchange = function () {
+        var file = this.files[0];
+
+        var reader = new FileReader();
+        reader.onload = function () {
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+
+      input.click();
+    }"
+  end
+
   # Specify a custom image to be used in place of the site title for mobile and
   # expanded/desktop navigation. These images should be placed within your
   # asset paths, e.g. app/assets/images.
